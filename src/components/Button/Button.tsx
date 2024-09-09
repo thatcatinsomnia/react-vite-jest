d8a451c5ee087c19bf5dd9f128e969c3378410ff
unknown <bennett.shen@ehsn.com.tw>
2024-09-09 16:07:12 +0800

    button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,12 +3,16 @@ import { type ReactNode } from 'react';
 type Props = {
   children: ReactNode;
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-export default function Button({ children, onClick }: Props) {
+export default function Button({ children, onClick, disabled = true }: Props) {
   return (
     <button 
-      className="mt-auto py-2 w-full bg-slate-700 hover:bg-slate-800 text-gray-50 rounded"
+      className={`
+        mt-auto py-2 w-full bg-gray-700 hover:bg-slate-800 transition-colors text-gray-50 rounded 
+        ${disabled ? 'opacity-20 pointer-events-none' : ''}
+      `}
       onClick={onClick}
     >
       {children}
